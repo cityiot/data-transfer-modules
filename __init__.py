6bf9b0e531f9472dc631b3ea5ef4145fb8ec5c7e
Jani Yli-Kantola <jani.ylikantola@gmail.com>
2018-11-29 13:11:51 +0200

    Archiving things

--- __init__.py
+++ __init__.py
@@ -7,7 +7,7 @@ __credits__ = [""]
 __license__ = "MIT"
 __version__ = "0.0.1"
 __maintainer__ = "Jani Yli-Kantola"
-__contact__ = "https://bitbucket.org/jylika/hieronnan-potilastietojarjestelma-2/"
+__contact__ = "https://bitbucket.org/cityiot/cityiot-platform-fiware/src/master/"
 __status__ = "Development"
 """
 
